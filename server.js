37a5d6d6a6fc3d185576fdcdfbd8c359f4606d2e
Manon5 <manon.touba05@gmail.com>
2020-06-03 11:17:51 +0200

    nettoyage du code
    remplacement des var par let ou const
    retiré l'imbrication des fonctions, déclaration des fonctions au début du programme

--- server.js
+++ server.js
@@ -1,67 +1,89 @@
-var express = require('express');
-var MongoClient = require("mongodb").MongoClient;
-
-var app = express();
-
+const express = require('express');
+const MongoClient = require("mongodb").MongoClient;
+const app = express();
 
 
-app.get('/', function(req, res) {
-    // extraction des données depuis mongoDB
-    MongoClient.connect("mongodb://localhost:27017", function(error, client) {
-            if (error) throw error;
-            var db = client.db('adopte_un_chaton');
-        db.collection("chats").find().toArray(function (error, results) {
-            if (error) throw error;
-            console.log(results.length);
-            res.render('home.ejs', {bdd : results});
-        });
-        console.log("Connecté à la base de données des chats");
-    });
+
+// affiche la totalité de la BDD sur la page d'accueil
+function displayHome(req, res){
+  // extraction des données depuis mongoDB
+  db.collection("chats").find().toArray(function (error, results) {
+      if (error) throw error;
+      res.render('home.ejs', {bdd : results});
+  });
+      console.log("Connecté à la base de données des chats");
+}
+
+//affiche le résultat d'une recherche par nom
+function displayRechercheNom(req, res){
+  // extraction des données depuis mongoDB
+  client.connect(function(error, client) {
+          if (error) throw error;
+          const db = client.db('adopte_un_chaton');
+      db.collection("chats").find({nom : req.param("nom")}).toArray(function (error, results) {
+          if (error) throw error;
+          res.render('home.ejs', {bdd : results});
+      });
+      console.log("Connecté à la base de données des chats");
+  });
+}
+
+//affiche le résultat d'une recherche par couleur
+function displayRechercheCouleur(req, res){
+  // extraction des données depuis mongoDB
+  client.connect(function(error, client) {
+          if (error) throw error;
+          const db = client.db('adopte_un_chaton');
+      db.collection("chats").find({couleur : req.param("couleur")}).toArray(function (error, results) {
+          if (error) throw error;
+          res.render('home.ejs', {bdd : results});
+      });
+      console.log("Connecté à la base de données des chats");
+  });
+}
+
+//ajoute un chat dans la BDD
+function addChat(req, res){
+  // extraction des données depuis mongoDB
+  client.connect(function(error, client) {
+          if (error) throw error;
+          const db = client.db('adopte_un_chaton');
+          let myobj = { nom: req.param("nom"), annee_nais: req.param("annee_nais"), couleur: req.param("couleur"),
+          annee_adopt : req.param("annee_adopt"), proprietaire: {nom: req.param("nom_prop"), prenom: req.param("prenom_prop"),
+          annee_nais: req.param("annee_nais_prop"), adresse: {numero: req.param("numero"), rue: req.param("rue"), ville: req.param("ville") }} };
+
+          db.collection("chats").insertOne(myobj, function(err, res) {
+            if (err) throw err;
+            console.log("1 document inserted");
+            db.close();
+         });
+      console.log("Connecté à la base de données des chats");
+  });
+}
+
+// fonction principale avec les routes
+function startApp(){
+  app.get('/', displayHome);
+  app.get('/rechercheNom', displayRechercheNom);
+  app.get('/rechercheCouleur', displayRechercheCouleur);
+  app.get('/ajout', addChat);
+  app.listen(8080);
+}
+
+
+// --------- Connexion à la BDD ----------- //
+// on instancie un client
+const client = new MongoClient('mongodb://localhost:27017', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
-
-app.get('/rechercheNom', function(req, res) {
-    // extraction des données depuis mongoDB
-    MongoClient.connect("mongodb://localhost:27017", function(error, client) {
-            if (error) throw error;
-            var db = client.db('adopte_un_chaton');
-        db.collection("chats").find({nom : req.param("nom")}).toArray(function (error, results) {
-            if (error) throw error;
-            res.render('home.ejs', {bdd : results});
-        });
-        console.log("Connecté à la base de données des chats");
-    });
+// on initialise la connexion
+client.connect(function(error){
+    if(error) {
+        throw error;
+    } else {
+        startApp();
+    }
 });
-
-app.get('/rechercheCouleur', function(req, res) {
-    // extraction des données depuis mongoDB
-    MongoClient.connect("mongodb://localhost:27017", function(error, client) {
-            if (error) throw error;
-            var db = client.db('adopte_un_chaton');
-        db.collection("chats").find({couleur : req.param("couleur")}).toArray(function (error, results) {
-            if (error) throw error;
-            res.render('home.ejs', {bdd : results});
-        });
-        console.log("Connecté à la base de données des chats");
-    });
-});
-
-app.get('/ajout', function(req, res) {
-    // extraction des données depuis mongoDB
-    MongoClient.connect("mongodb://localhost:27017", function(error, client) {
-            if (error) throw error;
-            var db = client.db('adopte_un_chaton');
-            var myobj = { nom: req.param("nom"), annee_nais: req.param("annee_nais"), couleur: req.param("couleur"),
-            annee_adopt : req.param("annee_adopt"), proprietaire: {nom: req.param("nom_prop"), prenom: req.param("prenom_prop"),
-            annee_nais: req.param("annee_nais_prop"), adresse: {numero: req.param("numero"), rue: req.param("rue"), ville: req.param("ville") }} };
-
-            db.collection("chats").insertOne(myobj, function(err, res) {
-              if (err) throw err;
-              console.log("1 document inserted");
-              db.close();
-           });
-        console.log("Connecté à la base de données des chats");
-    });
-});
-
-
-app.listen(8080);
+//accès à la base de données
+const db = client.db('adopte_un_chaton');
